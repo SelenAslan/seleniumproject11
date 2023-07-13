127ad2015e04073469480ea6d78c3ff48a70dbba
harun <hekinci@altayer.com>
2023-07-13 15:14:15 +0300

    day5 TestNG_Intro

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -0,0 +1,28 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("--> Before method is running!");
+
+    }
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("--> After method is running!");
+    }
+    @Test (priority = 2)
+    public void test1(){
+        System.out.println("Test 1 running..");
+
+    }
+    @Test (priority = 1)
+    public void test2(){
+        System.out.println("Test 2 running..");
+
+    }
+
+}
