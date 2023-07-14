b8031e1a5e1ab3bb8bc054b5502a7af94048f194
harun <hekinci@altayer.com>
2023-07-14 10:13:44 +0300

    day5 TestNG_Intro completed

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,10 +1,13 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNG_Intro {
+
+
     @BeforeMethod
     public void setUpMethod(){
         System.out.println("--> Before method is running!");
@@ -14,15 +17,27 @@ public class TestNG_Intro {
     public void tearDownMethod(){
         System.out.println("--> After method is running!");
     }
-    @Test (priority = 2)
+    @Test (priority = 1)
     public void test1(){
         System.out.println("Test 1 running..");
+        String actual = "apple";
+        String expected = "apple";
+
+        Assert.assertEquals(actual, expected);
 
     }
-    @Test (priority = 1)
+    @Test (priority = 2)
     public void test2(){
         System.out.println("Test 2 running..");
+        //AssertTrue
+        String actual = "apple2";
+        String expected = "apple";
+
+        Assert.assertTrue(actual.equals(expected));
+
 
     }
+
+
 
 }
