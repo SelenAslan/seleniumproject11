38d6d9b5e717704e27df72dfa6f0c4574938a535
harun <hekinci@altayer.com>
2023-07-15 18:35:03 +0300

    Windows task practice

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -62,8 +63,11 @@ public class T5_Windows_Practice {
         System.out.println("Title after click:" + actualTitle);
 
 
+    }
 
-
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
     }
 
 }
