1de596c66d461fcfa7d0322441215c6572015fc6
harun <hekinci@altayer.com>
2023-07-21 13:31:36 +0300

    last changes

--- src/test/java/com/cydeo/tests/day8_proporties_config_reader/T4_Config_Practice.java
+++ src/test/java/com/cydeo/tests/day8_proporties_config_reader/T4_Config_Practice.java
@@ -1,40 +1,41 @@
 package com.cydeo.tests.day8_proporties_config_reader;
 
 import com.cydeo.utilities.ConfigurationReader;
-import com.cydeo.utilities.WebDriverFactory_2;
+import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class T4_Config_Practice {
 
-    WebDriver driver;
-    @BeforeMethod
-    public void setupMethod() {
+   // WebDriver driver;
+    //    @BeforeMethod
+    //    public void setupMethod() {
+    //
+    //        //We are getting the browserType dynamically from our configuration.properties file
+    //       String browserType = ConfigurationReader.getProperty("browser");
+    //       driver = WebDriverFactory_2.getSite("https://www.google.com", browserType);
+    //
+    //    }
 
-        //We are getting the browserType dynamically from our configuration.properties file
-       String browserType = ConfigurationReader.getProperty("browser");
-       driver = WebDriverFactory_2.getSite("https://www.google.com", browserType);
 
-    }
 
     @Test
     public void google_search_test(){
 
+        Driver.getDriver().get("https://www.google.com");
 
         //3- Write “apple” in search box
-        WebElement googleSearchBox = driver.findElement(By.xpath("//textarea[@type='search']"));
+        WebElement googleSearchBox = Driver.getDriver().findElement(By.xpath("//textarea[@type='search']"));
 
         googleSearchBox.sendKeys(ConfigurationReader.getProperty("searchValue") + Keys.ENTER);
 
         //4- Verify title:
         //Expected: apple - Google Search
         String expectedTitle = ConfigurationReader.getProperty("searchValue")+" - Google'da Ara";
-        String actualTitle = driver.getTitle();
+        String actualTitle = Driver.getDriver().getTitle();
 
         Assert.assertEquals(actualTitle, expectedTitle);
 
